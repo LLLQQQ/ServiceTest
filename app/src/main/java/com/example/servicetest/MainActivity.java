1b355f11e7f56d2133a4d00bdbde64a0b5e98979
LLLQQQ <1170494354@qq.com>
2017-02-02 15:03:02 +0800

    first commit

---
+++ app/src/main/java/com/example/servicetest/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.servicetest;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button startservice;
+    private Button stopservice;
+
+    private Button bindService;
+    private Button unbindService;
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startservice = (Button) findViewById(R.id.start_service);
+        stopservice = (Button) findViewById(R.id.stop_service);
+        startservice.setOnClickListener(this);
+        stopservice.setOnClickListener(this);
+
+        bindService = (Button) findViewById(R.id.bind_service);
+        unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
